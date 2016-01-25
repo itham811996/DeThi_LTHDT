6cbde93ce253c4866c8fcd629d2150668e6e4262
Phuc Nguyen Minh <minhphuc429@gmail.com>
2016-01-25 20:32:42 +0700

    phuc

---
+++ LTHDT_BaiTap/Nguoi.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTHDT_BaiTap
+{
+    public abstract class Nguoi
+    {
+        string hoTen;
+        int namSinh;
+        string bangCap;
+
+        public Nguoi()
+        {
+            this.hoTen = "";
+            this.namSinh = 1995;
+            this.bangCap = "";
+        }
+
+        public Nguoi(string hoTen, int namSinh, string bangCap)
+        {
+            this.hoTen = hoTen;
+            this.namSinh = namSinh;
+            this.bangCap = bangCap;
+        }
+
+        public string HoTen
+        {
+            get
+            {
+                return this.hoTen;
+            }
+            set
+            {
+                this.hoTen = value;
+            }
+        }
+
+        public int NamSinh
+        {
+            get
+            {
+                return this.namSinh;
+            }
+            set
+            {
+                this.namSinh = value;
+            }
+        }
+
+        public string BangCap
+        {
+            get
+            {
+                return this.bangCap;
+            }
+            set
+            {
+                this.bangCap = value;
+            }
+        }
+
+        public virtual void Nhap()
+        {
+            Console.Write("Nhap Ho Ten: ");
+            HoTen = Console.ReadLine();
+            Console.Write("Nhap Nam Sinh: ");
+            NamSinh = int.Parse(Console.ReadLine());
+            Console.Write("Nhap Bang Cap: ");
+            BangCap = Console.ReadLine();
+        }
+
+        public virtual void Xuat()
+        {
+            Console.Write("{0,20} {1,4} {2,20}",HoTen,NamSinh,BangCap);
+        }
+    }
+}
---
+++ LTHDT_BaiTap/NhaQuanLy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTHDT_BaiTap
+{
+    public class NhaQuanLy:Nguoi
+    {
+        string chucVu;
+        float soNgayCong;
+        float bacLuong;
+
+        public NhaQuanLy()
+            : base()
+        {
+            this.chucVu = "";
+            this.soNgayCong = 0.0F;
+            this.bacLuong = 0.0F;
+        }
+
+        public NhaQuanLy(string hoTen, int namSinh, string bangCap, string chucVu, float soNgayCong, float bacLuong)
+            : base(hoTen,namSinh,bangCap)
+        {
+            this.chucVu = chucVu;
+            this.soNgayCong = soNgayCong;
+            this.bacLuong = bacLuong;
+        }
+
+        public string ChucVu
+        {
+            get
+            {
+                return this.chucVu;
+            }
+            set
+            {
+                this.chucVu = value;
+            }
+        }
+
+        public float SoNgayCong
+        {
+            get
+            {
+                return this.soNgayCong;
+            }
+            set
+            {
+                this.soNgayCong = value;
+            }
+        }
+
+        public float BacLuong
+        {
+            get
+            {
+                return this.bacLuong;
+            }
+            set
+            {
+                this.bacLuong = value;
+            }
+        }
+
+        public override void Nhap()
+        {
+            base.Nhap();
+            Console.Write("Nhap Chuc Vu: ");
+            ChucVu = Console.ReadLine();
+            Console.Write("Nhap So Ngay Cong: ");
+            SoNgayCong = float.Parse(Console.ReadLine());
+            Console.Write("Nhap Bac Luong: ");
+            BacLuong = float.Parse(Console.ReadLine());
+        }
+
+        public override void Xuat()
+        {
+            base.Xuat();
+            Console.Write("{0,20} {1,2:2F} {2,3:2F}", ChucVu, SoNgayCong, BacLuong);
+        }
+    }
+}
